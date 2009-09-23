4ad792cb80832e9a6386ab72811f43fdbf98b5ad
tsujikawa <ttsujikawa@gmail.com>
2009-09-23 18:38:50 +0900

    index page を移植した.

--- dic/dic.py
+++ dic/dic.py
@@ -3,11 +3,13 @@ import cgi
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from dic.controller.api import WordPage,WordsPage
+from dic.controller.index import IndexPage
 
 application = webapp.WSGIApplication(
                                      [
                                       ('/dic/api/words', WordsPage),
                                       ('/dic/api/word/', WordPage),
+                                      ('/dic/', IndexPage),
                                      ],
                                      debug=True)
 

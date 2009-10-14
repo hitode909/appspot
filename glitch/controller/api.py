6c5fcf8fe5e59142c2e00c5f45104d583ac14e98
hitode909 <hitode909@gmail.com>
2009-10-14 23:23:49 +0900

    error handling, convert to jpeg

--- glitch/controller/api.py
+++ glitch/controller/api.py
@@ -1,16 +1,46 @@
 import logging
 from google.appengine.ext import webapp
 from google.appengine.ext import db
-from google.appengine.api import memcache, urlfetch
+from google.appengine.api import memcache, urlfetch, images
 from django.utils import simplejson
 import base64
 import random
+import hashlib
+import re
 
 class ApiPage(webapp.RequestHandler):
     def get(self):
         uri = self.request.get('uri')
-        image = self.get_image(uri)
-        
+        try:
+            image = self.get_image(uri)
+        except (urlfetch.InvalidURLError), inst:
+            self.response.set_status(400)
+            if self.request.get('uri'):
+                self.response.out.write("uri is invalid.")
+            else:
+                self.response.out.write("uri is required.")
+            return
+        except (urlfetch.DownloadError), inst:
+            self.response.set_status(400)
+            self.response.out.write("failed to download.")
+            return
+        if image['status_code'] != 200:
+            self.response.set_status(400)
+            self.response.out.write("failed to download.")
+            return
+        if not re.compile("^image").match(image['headers']['content-type']):
+            self.response.set_status(400)
+            self.response.out.write("not image")
+            return
+
+        if image['headers']['content-type'] != 'image/jpeg':
+            converter = images.Image(image['content'])
+            converter.rotate(0)
+            image['content'] = converter.execute_transforms(output_encoding=images.JPEG)
+            image['headers']['content-type'] = 'image/jpeg'
+            self.save_image(uri, image)
+            logging.info("image converted (%s)" % (uri))
+
         keys = self.request.arguments()
         keys.remove('uri')
         glitch_rule = {}
@@ -18,14 +48,14 @@ class ApiPage(webapp.RequestHandler):
         if len(glitch_rule) == 0:
             glitch_rule = self.random_glitch_rule()
         glitched_content = self.glitch(image['content'], glitch_rule)
+
+
             
         self.response.headers['Content-Type'] = image['headers']['content-type']
         self.response.out.write(glitched_content)
         return
 
     def get_image(self, uri):
-        if not uri: raise Exception, '/glitch/api?uri=URI&FROM=TO' 
-        
         image = self.load_image(uri)
         if image: return image
 
@@ -37,7 +67,7 @@ class ApiPage(webapp.RequestHandler):
         image = urlfetch.fetch(uri)
         headers = {}
         for key in image.headers.keys(): headers[key] = image.headers[key]
-            
+        
         return {"content": image.content, "status_code": image.status_code, "headers": headers}
     
     def load_image(self, uri):
@@ -56,8 +86,12 @@ class ApiPage(webapp.RequestHandler):
 
     def random_glitch_rule(self):
         rule = {}
-        for i in range(int(random.random()*3+1)):
-            rule[str(int(random.random()*100))] = str(int(random.random()*100))
+        seed = hashlib.sha1(str(random.random())).hexdigest()
+        for i in range(int(random.random()*2+1)):
+            a = int(random.random()*3+1)
+            b = int(random.random()*3+1)
+            rule[seed[:a]] = seed[a:a+b]
+            seed = seed[a+b:]
         logging.debug("random rule generated(%s)", str(rule))
         return rule
         

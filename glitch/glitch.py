74544517c32d5aae6eea72b50e8751cb83aa2b5d
hitode909 <hitode909@gmail.com>
2010-03-01 23:01:40 +0900

    api2

--- glitch/glitch.py
+++ glitch/glitch.py
@@ -3,10 +3,12 @@ import cgi
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from glitch.controller.api import ApiPage
+from glitch.controller.api2 import Api2Page
 
 application = webapp.WSGIApplication(
                                      [
                                       ('/glitch/api', ApiPage),
+                                      ('/glitch/api2', Api2Page),
                                      ],
                                      debug=True)
 

134d3a6984bd8b317a582077f8494e749c16e1c3
hitode909 <hitode909@gmail.com>
2011-10-21 22:27:28 +0900

    visualizer

--- htdocs/one-liner-music/one-liner-music.coffee
+++ htdocs/one-liner-music/one-liner-music.coffee
@@ -1,30 +1,49 @@
-convert = (v) ->
-  (v % 256) / 128 - 0.5
+sample_8bit_to_float = (v) ->
+  v / 128 - 0.5
 
 play = ->
   channel = 2
   stream_length = 4096
   context = new webkitAudioContext()
   node = context.createJavaScriptNode(stream_length, 1, channel)
-  console.log(node)
-  t2 = 0
+  time_count = 0
+  last_t = 0
+  value = 0
+  plot_at = 0
+  sample_value = 0.0
+  canvas = $('canvas#plot')
+  canvas_context = canvas[0].getContext('2d')
+  canvas_width = canvas.width()
+  canvas_height = canvas.height()
+  fun = -> 0
+
+  update_sample = (t)->
+
+    value = fun(t) % 255
+    sample_value = sample_8bit_to_float(value)
+
+    plot_at++
+    canvas_context.fillStyle = "rgb(#{value}, 0, 0)"
+    x = Math.floor(plot_at / canvas_width) % canvas_width
+    y = plot_at % canvas_height
+    canvas_context.fillRect(x, y, 1, 1)
 
   node.onaudioprocess = (event) ->
     data = event.outputBuffer.getChannelData(0)
     len = data.length
-
-    fun = -> 0
-
+    sampling_rate = $('input[name="sampling-rate"]:checked').val()
     try
       v = $("#f").val()
       fun = eval("(function(t){return " + v + ";})")
 
-    sampling_rate = $('input[name="sampling-rate"]:checked').val()
 
     for i in [0..len]
-      t = Math.floor(t2 * sampling_rate / 44100)
-      data[i] = convert(fun(t))
-      t2++
+      t = Math.floor(time_count * sampling_rate / 44100)
+      if t != last_t
+        update_sample(t)
+        last_t = t
+      data[i] = sample_value
+      time_count++
 
   node.connect(context.destination)
 
--- htdocs/one-liner-music/one-liner-music.js
+++ htdocs/one-liner-music/one-liner-music.js
@@ -1,32 +1,53 @@
-var convert, play;
-convert = function(v) {
-  return (v % 256) / 128 - 0.5;
+var play, sample_8bit_to_float;
+sample_8bit_to_float = function(v) {
+  return v / 128 - 0.5;
 };
 play = function() {
-  var channel, context, node, stream_length, t2;
+  var canvas, canvas_context, canvas_height, canvas_width, channel, context, fun, last_t, node, plot_at, sample_value, stream_length, time_count, update_sample, value;
   channel = 2;
   stream_length = 4096;
   context = new webkitAudioContext();
   node = context.createJavaScriptNode(stream_length, 1, channel);
-  console.log(node);
-  t2 = 0;
+  time_count = 0;
+  last_t = 0;
+  value = 0;
+  plot_at = 0;
+  sample_value = 0.0;
+  canvas = $('canvas#plot');
+  canvas_context = canvas[0].getContext('2d');
+  canvas_width = canvas.width();
+  canvas_height = canvas.height();
+  fun = function() {
+    return 0;
+  };
+  update_sample = function(t) {
+    var x, y;
+    value = fun(t) % 255;
+    sample_value = sample_8bit_to_float(value);
+    plot_at++;
+    canvas_context.fillStyle = "rgb(" + value + ", 0, 0)";
+    x = Math.floor(plot_at / canvas_width) % canvas_width;
+    y = plot_at % canvas_height;
+    return canvas_context.fillRect(x, y, 1, 1);
+  };
   node.onaudioprocess = function(event) {
-    var data, fun, i, len, sampling_rate, t, v, _results;
+    var data, i, len, sampling_rate, t, v, _results;
     data = event.outputBuffer.getChannelData(0);
     len = data.length;
-    fun = function() {
-      return 0;
-    };
+    sampling_rate = $('input[name="sampling-rate"]:checked').val();
     try {
       v = $("#f").val();
       fun = eval("(function(t){return " + v + ";})");
     } catch (_e) {}
-    sampling_rate = $('input[name="sampling-rate"]:checked').val();
     _results = [];
     for (i = 0; 0 <= len ? i <= len : i >= len; 0 <= len ? i++ : i--) {
-      t = Math.floor(t2 * sampling_rate / 44100);
-      data[i] = convert(fun(t));
-      _results.push(t2++);
+      t = Math.floor(time_count * sampling_rate / 44100);
+      if (t !== last_t) {
+        update_sample(t);
+        last_t = t;
+      }
+      data[i] = sample_value;
+      _results.push(time_count++);
     }
     return _results;
   };

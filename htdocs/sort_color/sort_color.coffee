a14064fe2e039f7783933a13c1cbe5c39419b1de
hitode909 <hitode909@gmail.com>
2012-04-13 20:36:13 +0900

    up

--- htdocs/sort_color/sort_color.coffee
+++ htdocs/sort_color/sort_color.coffee
@@ -1,10 +1,15 @@
 $ ->
 
+  num_to_color = (num) ->
+    '#' + ('000000' + (+num).toString(16))[-6..-1]
+
   load_img_to_canvas = (img) ->
     item_container = $('<div>')
       .addClass 'item'
 
-    $('body').append(item_container)
+    $('#image-container')
+      .empty()
+      .append(item_container)
 
     $canvas = $('<canvas>')
       .addClass('image')
@@ -40,52 +45,95 @@ $ ->
       return 0 if a[1] == b[1]
       if a[1] < b[1] then 1 else -1
 
+
+    displayed_colors_length = 0
+
+    base = canvas.width * canvas.height
+
     for color in famous_colors
       width = color[1] * 5  / canvas.width
       break if width < 1
+      displayed_colors_length++
       $('<span>')
         .addClass('color')
         .attr
-          title: '#' + (+color[0]).toString(16) + " (#{ color[1] })"
-          'data-color': '#' + (+color[0]).toString(16)
+          'data-color': num_to_color(color[0])
         .css
           display: 'inline-block',
           width: width
           height: canvas.height
-          background: '#' + (+color[0]).toString(16)
+          background: num_to_color(color[0])
         .appendTo(container)
+
+    console.log "displayed #{ displayed_colors_length / famous_colors.length }, #{ displayed_colors_length} of  #{ famous_colors.length }"
 
   $(document).bind 'dragover', ->
     false
 
   $(document).bind 'drop', (jquery_event) ->
     event = jquery_event.originalEvent
-    for file in  event.dataTransfer.files
-      do ->
-        reader = new FileReader
-        reader.onload = ->
-          img = new Image
-          img.onload = ->
-            histogram(load_img_to_canvas img)
-          img.src = reader.result
+    file = event.dataTransfer.files[0]
+    reader = new FileReader
+    reader.onload = ->
+      img = new Image
+      img.onload = ->
+        histogram(load_img_to_canvas img)
+      img.src = reader.result
 
-        reader.readAsDataURL file
+    reader.readAsDataURL file
 
     false
 
+  pick_color = (color) ->
+    color_item =  $('<div>')
+        .append(
+          $('<span>')
+            .addClass('color-sample')
+            .css
+              background: color
+        ).append(color)
+
+    $('#selected-colors').append color_item
+
   setup_click_color = ->
-    $('body').on 'click', '.color', ->
+    $('body').on 'click', '.color', (event) ->
       color = $(this).attr('data-color')
-      container = $(this).parents('.item')
-      container.append(
-        $('<div>')
-          .append(
-            $('<span>')
-              .addClass('color-sample')
-              .css
-                background: color
-          ).append(color)
-      )
+      pick_color(color)
 
   setup_click_color()
+
+  setup_cursor = ->
+    offset = 15
+    bg_color = '#ffffff'
+    $(document).bind 'mousemove', (event)->
+      $('.cursor-preview').remove()
+      $('<span>').addClass('cursor-preview').appendTo($('body')).css
+        left: event.pageX + offset
+        top: event.pageY + offset
+        'background-color': bg_color
+
+      true
+
+    $(document).on 'mousemove', '.color', (event)->
+      bg_color = $(event.target).attr('data-color')
+      true
+
+    get_color_from_canvas = (canvas, x, y) ->
+      ctx = canvas.getContext('2d')
+      data = ctx.getImageData(x, y, 1, 1).data
+      v = (data[0] << 16) + (data[1] << 8) + (data[2])
+      num_to_color(v)
+
+    $(document).on 'mousemove', 'canvas', (event)->
+      canvas = event.target
+      bg_color = get_color_from_canvas(canvas, event.offsetX, event.offsetY)
+      true
+
+    $(document).on 'click', 'canvas', (event) ->
+      canvas = event.target
+      color = get_color_from_canvas(canvas, event.offsetX, event.offsetY)
+      pick_color(color)
+
+
+  setup_cursor()
 
--- htdocs/sort_color/sort_color.js
+++ htdocs/sort_color/sort_color.js
@@ -1,9 +1,12 @@
 $(function() {
-  var histogram, load_img_to_canvas, setup_click_color;
+  var histogram, load_img_to_canvas, num_to_color, pick_color, setup_click_color, setup_cursor;
+  num_to_color = function(num) {
+    return '#' + ('000000' + (+num).toString(16)).slice(-6);
+  };
   load_img_to_canvas = function(img) {
     var $canvas, canvas, ctx, item_container;
     item_container = $('<div>').addClass('item');
-    $('body').append(item_container);
+    $('#image-container').empty().append(item_container);
     $canvas = $('<canvas>').addClass('image');
     item_container.append($canvas);
     canvas = $canvas[0];
@@ -14,7 +17,7 @@ $(function() {
     return item_container;
   };
   histogram = function(container) {
-    var canvas, color, count, ctx, data, famous_colors, i, img_data, len, list, table, v, width, _i, _len, _ref, _results;
+    var base, canvas, color, count, ctx, data, displayed_colors_length, famous_colors, i, img_data, len, list, table, v, width, _i, _len, _ref;
     canvas = container.find('canvas')[0];
     ctx = canvas.getContext('2d');
     img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -45,60 +48,96 @@ $(function() {
         return -1;
       }
     });
-    _results = [];
+    displayed_colors_length = 0;
+    base = canvas.width * canvas.height;
     for (_i = 0, _len = famous_colors.length; _i < _len; _i++) {
       color = famous_colors[_i];
       width = color[1] * 5 / canvas.width;
       if (width < 1) {
         break;
       }
-      _results.push($('<span>').addClass('color').attr({
-        title: '#' + (+color[0]).toString(16) + (" (" + color[1] + ")"),
-        'data-color': '#' + (+color[0]).toString(16)
+      displayed_colors_length++;
+      $('<span>').addClass('color').attr({
+        'data-color': num_to_color(color[0])
       }).css({
         display: 'inline-block',
         width: width,
         height: canvas.height,
-        background: '#' + (+color[0]).toString(16)
-      }).appendTo(container));
+        background: num_to_color(color[0])
+      }).appendTo(container);
     }
-    return _results;
+    return console.log("displayed " + (displayed_colors_length / famous_colors.length) + ", " + displayed_colors_length + " of  " + famous_colors.length);
   };
   $(document).bind('dragover', function() {
     return false;
   });
   $(document).bind('drop', function(jquery_event) {
-    var event, file, _fn, _i, _len, _ref;
+    var event, file, reader;
     event = jquery_event.originalEvent;
-    _ref = event.dataTransfer.files;
-    _fn = function() {
-      var reader;
-      reader = new FileReader;
-      reader.onload = function() {
-        var img;
-        img = new Image;
-        img.onload = function() {
-          return histogram(load_img_to_canvas(img));
-        };
-        return img.src = reader.result;
+    file = event.dataTransfer.files[0];
+    reader = new FileReader;
+    reader.onload = function() {
+      var img;
+      img = new Image;
+      img.onload = function() {
+        return histogram(load_img_to_canvas(img));
       };
-      return reader.readAsDataURL(file);
+      return img.src = reader.result;
     };
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      file = _ref[_i];
-      _fn();
-    }
+    reader.readAsDataURL(file);
     return false;
   });
+  pick_color = function(color) {
+    var color_item;
+    color_item = $('<div>').append($('<span>').addClass('color-sample').css({
+      background: color
+    })).append(color);
+    return $('#selected-colors').append(color_item);
+  };
   setup_click_color = function() {
-    return $('body').on('click', '.color', function() {
-      var color, container;
+    return $('body').on('click', '.color', function(event) {
+      var color;
       color = $(this).attr('data-color');
-      container = $(this).parents('.item');
-      return container.append($('<div>').append($('<span>').addClass('color-sample').css({
-        background: color
-      })).append(color));
+      return pick_color(color);
     });
   };
-  return setup_click_color();
+  setup_click_color();
+  setup_cursor = function() {
+    var bg_color, get_color_from_canvas, offset;
+    offset = 15;
+    bg_color = '#ffffff';
+    $(document).bind('mousemove', function(event) {
+      $('.cursor-preview').remove();
+      $('<span>').addClass('cursor-preview').appendTo($('body')).css({
+        left: event.pageX + offset,
+        top: event.pageY + offset,
+        'background-color': bg_color
+      });
+      return true;
+    });
+    $(document).on('mousemove', '.color', function(event) {
+      bg_color = $(event.target).attr('data-color');
+      return true;
+    });
+    get_color_from_canvas = function(canvas, x, y) {
+      var ctx, data, v;
+      ctx = canvas.getContext('2d');
+      data = ctx.getImageData(x, y, 1, 1).data;
+      v = (data[0] << 16) + (data[1] << 8) + data[2];
+      return num_to_color(v);
+    };
+    $(document).on('mousemove', 'canvas', function(event) {
+      var canvas;
+      canvas = event.target;
+      bg_color = get_color_from_canvas(canvas, event.offsetX, event.offsetY);
+      return true;
+    });
+    return $(document).on('click', 'canvas', function(event) {
+      var canvas, color;
+      canvas = event.target;
+      color = get_color_from_canvas(canvas, event.offsetX, event.offsetY);
+      return pick_color(color);
+    });
+  };
+  return setup_cursor();
 });

177df7d8aa7bb4f165020f9242a4f8fcd7773485
hitode909 <hitode909@gmail.com>
2013-01-01 11:59:50 +0900

    add 2013

---
+++ htdocs/2013/2013.coffee
@@ -0,0 +1,77 @@
+getArgs = ->
+  args =
+    strokeWeight: + $('#stroke-weight').val()
+    radiusDiff: + $('#radius-diff').val()
+    radianDiff: + $('#radian-diff').val()
+    radiusNoiseDiff: + $('#radius-noise-diff').val()
+
+saveState = ->
+  args = getArgs()
+
+  hash = [args.strokeWeight, args.radiusDiff, args.radianDiff, args.radiusNoiseDiff].join(',')
+  if '#' + hash != location.hash
+    location.hash = hash
+
+render = (processing) ->
+
+  processing.setup = ->
+    processing.size(500, 500)
+    processing.frameRate(12)
+
+  processing.draw = ->
+    center =
+      x: processing.width / 2
+      y: processing.height / 2
+
+    args = getArgs()
+    processing.background(255)
+    processing.stroke(255, 0, 0)
+    processing.smooth()
+    processing.strokeWeight(args.strokeWeight*10)
+
+    lastX = null
+    lastY = null
+    radian = 0.0
+    radius = 1.0
+    radiusNoiseTime = 0.0
+    time = 0
+    currentRadius = 0
+    while ++time < 5000 and Math.abs(currentRadius) < Math.abs(center.x)
+      radius += args.radiusDiff
+      radian += args.radianDiff
+      radiusNoiseTime += args.radiusNoiseDiff
+
+      # with noise
+      currentRadius = radius * processing.noise(radiusNoiseTime)
+      currentX = center.x + Math.sin(radian) * currentRadius
+      currentY = center.y + Math.cos(radian) * currentRadius
+
+      if lastX and lastY
+        processing.line(lastX, lastY, currentX, currentY)
+
+      lastX = currentX
+      lastY = currentY
+
+    # saveState()
+
+save = ->
+  dataURL = $('#screen').get(0).toDataURL()
+
+  $.ajax
+    async: false
+    type: 'POST'
+    url: '/png/'
+    data:
+      data: dataURL
+  .done (tinyURL) ->
+    console.log(tinyURL)
+    window.open "https://twitter.com/share?" + $.param(
+      url: 'http://hitode909.appspot.com/2013/',
+      text: "謹賀新年 #{tinyURL}"
+    )
+
+$ ->
+  new Processing($('#screen').get(0), render)
+
+  $('#save').click ->
+    save()
---
+++ htdocs/2013/2013.js
@@ -0,0 +1,82 @@
+var getArgs, render, save, saveState;
+getArgs = function() {
+  var args;
+  return args = {
+    strokeWeight: +$('#stroke-weight').val(),
+    radiusDiff: +$('#radius-diff').val(),
+    radianDiff: +$('#radian-diff').val(),
+    radiusNoiseDiff: +$('#radius-noise-diff').val()
+  };
+};
+saveState = function() {
+  var args, hash;
+  args = getArgs();
+  hash = [args.strokeWeight, args.radiusDiff, args.radianDiff, args.radiusNoiseDiff].join(',');
+  if ('#' + hash !== location.hash) {
+    return location.hash = hash;
+  }
+};
+render = function(processing) {
+  processing.setup = function() {
+    processing.size(500, 500);
+    return processing.frameRate(12);
+  };
+  return processing.draw = function() {
+    var args, center, currentRadius, currentX, currentY, lastX, lastY, radian, radius, radiusNoiseTime, time, _results;
+    center = {
+      x: processing.width / 2,
+      y: processing.height / 2
+    };
+    args = getArgs();
+    processing.background(255);
+    processing.stroke(255, 0, 0);
+    processing.smooth();
+    processing.strokeWeight(args.strokeWeight * 10);
+    lastX = null;
+    lastY = null;
+    radian = 0.0;
+    radius = 1.0;
+    radiusNoiseTime = 0.0;
+    time = 0;
+    currentRadius = 0;
+    _results = [];
+    while (++time < 5000 && Math.abs(currentRadius) < Math.abs(center.x)) {
+      radius += args.radiusDiff;
+      radian += args.radianDiff;
+      radiusNoiseTime += args.radiusNoiseDiff;
+      currentRadius = radius * processing.noise(radiusNoiseTime);
+      currentX = center.x + Math.sin(radian) * currentRadius;
+      currentY = center.y + Math.cos(radian) * currentRadius;
+      if (lastX && lastY) {
+        processing.line(lastX, lastY, currentX, currentY);
+      }
+      lastX = currentX;
+      _results.push(lastY = currentY);
+    }
+    return _results;
+  };
+};
+save = function() {
+  var dataURL;
+  dataURL = $('#screen').get(0).toDataURL();
+  return $.ajax({
+    async: false,
+    type: 'POST',
+    url: '/png/',
+    data: {
+      data: dataURL
+    }
+  }).done(function(tinyURL) {
+    console.log(tinyURL);
+    return window.open("https://twitter.com/share?" + $.param({
+      url: 'http://hitode909.appspot.com/2013/',
+      text: "謹賀新年 " + tinyURL
+    }));
+  });
+};
+$(function() {
+  new Processing($('#screen').get(0), render);
+  return $('#save').click(function() {
+    return save();
+  });
+});
